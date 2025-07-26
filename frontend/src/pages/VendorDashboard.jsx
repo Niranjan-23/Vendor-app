b85ed93243f3daa493ee111e0561fc32631d2b98
Niranjan <niranjan230525@gmail.com>
2025-07-26 13:30:11 +0000

    delete

--- frontend/src/pages/VendorDashboard.jsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from '@mui/material';
-
-export default function VendorDashboard() {
-  return (
-    <div style={{ padding: '1rem' }}>
-      <Typography variant="h5">Vendor Dashboard</Typography>
-    </div>
-  );
-}
